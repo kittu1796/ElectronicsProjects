db97ac990ed37aca42217dc536e8d30c43df0d47
Ashwini Kumar Gupta <kittu1796@gmail.com>
2019-07-18 01:20:02 +0530

    Created batch file for ProjectFolder structure creation

---
+++ 0. ProjectStrcture/FolderStructure.bat
@@ -0,0 +1,35 @@
+@echo off
+
+REM -Create ProjectFolder  
+
+md "n. ProjectName"
+cd "n. ProjectName"
+
+REM -Change directory to Project folder
+md "1. Design Consideration" "2. Software" "3. Hardware" "4. PCB" "5. CAD" "6. Results" 
+
+REM- Change directory to Software
+cd "2. Software"
+md "1. Working" "2. Final" "3. Archive"
+
+REM- Change directory to Hardware
+cd..
+cd "3. Hardware"
+md "1. Working" "2. Final" "3. Archive" "4. Component Datasheets"
+cd "1. Working"
+md "1. Design Calculations" "2. Practical Observation"
+
+REM - change directory to PCB
+
+REM - 
+
+REM - 
+
+REM - 
+cd \ 
+
+
+
+
+
+
